58e17f5d22917bebf43b6760067544ff1ba06810
ruiyigan <85063215+ruiyigan@users.noreply.github.com>
2022-01-13 22:19:56 +0800

    Error handling and abstraction of token authentication

--- app.js
+++ app.js
@@ -25,10 +25,10 @@ app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/login', loginRouter)
-
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,9 @@ const User = require('../models/user')
 
 usersRouter.post('/', async (request, response) => {
   const body = request.body
-
+  if (body.password.length < 3) {
+    return response.status(400).json({ error: 'password too short' })
+  }
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
